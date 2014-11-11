5e74a9b23910a885689a8fedb9b9b0aa5156e586
Justin Reidy <jmreidy@rzrsharp.net>
2014-11-11 14:26:32 -0800

    add web-mode to emacs

---
+++ emacs/live-packs.symlink/jmreidy-pack/config/webmode-conf.el
@@ -0,0 +1,4 @@
+(live-add-pack-lib "web-mode")
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
--- emacs/live-packs.symlink/jmreidy-pack/init.el
+++ emacs/live-packs.symlink/jmreidy-pack/init.el
@@ -5,3 +5,4 @@
 
 ;; Load bindings config
 (live-load-config-file "bindings.el")
+(live-load-config-file "webmode-conf.el")
